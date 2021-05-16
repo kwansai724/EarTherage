9bcc309e3581a02190e377ce82c1848435df67d6
ruffini47 <ruffini47@gmail.com>
2021-05-16 22:23:57 +0900

    db/seeds.rb作成、ログアウト後の遷移先設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
+
+  # studentにログイン後に遷移するpathを設定
   def student_after_sign_in_path_for(resource)
     therapist_training_course_path(resource)
   end
 
+  # staffにログイン後に遷移するpathを設定
   def staff_after_sign_in_path_for(resource)
     staffs_screen_path(resource)
+  end
+
+  # ログアウト後に遷移するpathを設定
+  def after_sign_out_path_for(resource)
+    if resource_name == :student
+      new_student_session_path
+    elsif resource_name == :staff
+      new_staff_session_path
+    end
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |n|
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  Student.create!(
+    email: email,
+    password: password,
+    password_confirmation: password,
+  )
+end
+
+puts "Student Created"
+
+10.times do |n|
+  email = "sample#{n+1}@email.com"
+  password = "password"
+  Staff.create!(
+    email: email,
+    password: password,
+    password_confirmation: password,
+  )
+end
+
+puts "Staff Created"
+
