35b853b584c65ff1f3e4eaadb16f4458b60898ca
kwansai724 <kwansai.taka724@gmail.com>
2021-05-23 14:55:46 +0900

    トップページ、マイページ画面デザイン・ルーティング修正

--- app/helpers/therapist_training_course_helper.rb
+++ app/helpers/therapist_training_course_helper.rb
@@ -1,8 +1,2 @@
 module TherapistTrainingCourseHelper
-  
-  def course_name(student)
-    return "セラピスト養成コース" if student.course_type == "therapist_training"
-    return "セルフケアコース" if student.course_type == "self_care"
-  end
-
 end
