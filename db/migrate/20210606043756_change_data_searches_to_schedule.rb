f25b8fc121348ce07538d5d44d272634739927b8
ruffini47 <ruffini47@gmail.com>
2021-06-15 20:56:09 +0900

    マイグレションファイル変更

--- db/migrate/20210606043756_change_data_searches_to_schedule.rb
+++ db/migrate/20210606043756_change_data_searches_to_schedule.rb
@@ -1,6 +1,6 @@
 class ChangeDataSearchesToSchedule < ActiveRecord::Migration[5.2]
   def change
-    change_column :schedules, :area, :integer
+    change_column :schedules, :area, :integer USING area::integer
     change_column :schedules, :event_type, :integer
     change_column :schedules, :teacher, :integer
   end
