4e6c39c0831281b83d31c4018295d5fddb643877
ruffini47 <ruffini47@gmail.com>
2021-06-15 21:13:50 +0900

    マイグレーションファイル変更3

--- db/migrate/20210606043756_change_data_searches_to_schedule.rb
+++ db/migrate/20210606043756_change_data_searches_to_schedule.rb
@@ -1,7 +1,7 @@
 class ChangeDataSearchesToSchedule < ActiveRecord::Migration[5.2]
   def change
     change_column :schedules, :area, 'integer USING CAST(area AS integer)'
-    change_column :schedules, :event_type, :integer
-    change_column :schedules, :teacher, :integer
+    change_column :schedules, :event_type, 'integer USING CAST(event_type AS integer)'
+    change_column :schedules, :teacher, 'integer USING CAST(teacher AS integer)'
   end
 end
