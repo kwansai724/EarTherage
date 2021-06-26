7568361e22c49be8ee4237a59cd33a26dab49539
ruffini47 <ruffini47@gmail.com>
2021-06-26 21:22:08 +0900

    spec/controllers/students_controller_spec.rbとspec/controllers/admins_controller_spec.rbの異常系を付け足す

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -54,8 +54,8 @@ RSpec.describe StudentsController, type: :controller do
         end
       end
 
-      context "受講生としてログインしたとき" do
-        context "as a student" do
+      context "セラピスト養成コースの受講生としてログインしたとき" do
+        context "as a therapist training course student" do
           before do
             student = FactoryBot.create(:student)
             sign_in student
@@ -73,6 +73,36 @@ RSpec.describe StudentsController, type: :controller do
               expect(response).to have_http_status "302"
             end
           end
+
+      context "セルフケアコースの受講生としてログインしたとき" do
+        context "as a self care coure student" do
+          before do
+            student = FactoryBot.create(:student, :self_care)
+            sign_in student
+            get :index
+          end
+
+          context "正常にレスポンスを返さないこと" do
+            it "responds not successfully" do
+              expect(response).not_to be_successful
+            end
+          end
+
+          context "302レスポンスを返すこと" do
+            it "returns a 302 response" do
+              expect(response).to have_http_status "302"
+            end
+          end
+
+          context "ルート画面にリダイレクトすること" do
+            it "redirects to the root page" do
+              expect(response).to redirect_to root_path
+            end
+          end
+        end
+      end
+
+
 
           context "ルート画面にリダイレクトすること" do
             it "redirects to the root page" do
@@ -163,8 +193,8 @@ RSpec.describe StudentsController, type: :controller do
         end
       end
 
-      context "受講生としてログインしたとき" do
-        context "as a student" do
+      context "セラピスト養成コースの受講生としてログインしたとき" do
+        context "as a therapist training course student" do
           before do
             student = FactoryBot.create(:student)
             sign_in student
@@ -183,6 +213,37 @@ RSpec.describe StudentsController, type: :controller do
               expect(response).to have_http_status "302"
             end
           end
+
+      context "セルフケアコースの受講生としてログインしたとき" do
+        context "as a self care course student" do
+          before do
+            student = FactoryBot.create(:student, :self_care)
+            sign_in student
+            another_student = FactoryBot.create(:student)
+            get :show, params: { id: another_student.id }
+          end
+
+          context "正常にレスポンスを返さないこと" do
+            it "responds not successfully" do
+              expect(response).not_to be_successful
+            end
+          end
+
+          context "302レスポンスを返すこと" do
+            it "returns a 302 response" do
+              expect(response).to have_http_status "302"
+            end
+          end
+
+          context "ルート画面にリダイレクトすること" do
+            it "redirects to the root page" do
+              expect(response).to redirect_to root_path
+            end
+          end
+        end
+      end
+
+
 
           context "ルート画面にリダイレクトすること" do
             it "redirects to the root page" do
@@ -287,8 +348,8 @@ RSpec.describe StudentsController, type: :controller do
         end
       end
 
-      context "受講生としてログインしたとき" do
-        context "as a student" do
+      context "セラピスト養成コースの受講生としてログインしたとき" do
+        context "as a therapist training course student" do
           before do
             student = FactoryBot.create(:student)
             sign_in student
@@ -318,6 +379,40 @@ RSpec.describe StudentsController, type: :controller do
           end
         end
       end
+
+      context "セルフケアコースの受講生としてログインしたとき" do
+        context "as a self care course student" do
+          before do
+            student = FactoryBot.create(:student, :self_care)
+            sign_in student
+            @another_student = FactoryBot.create(:student)
+            student_params = FactoryBot.attributes_for(:student, name: "abc")
+            patch :update, params: { id: @another_student.id, student: student_params }
+          end
+          context "abcに更新されないこと" do
+            it "not updated to abc" do
+              expect(@another_student.reload.name).to_not eq "abc"
+            end
+          end
+          context "正常にレスポンスを返さないこと" do
+            it "responds not successfully" do
+              expect(response).not_to be_successful
+            end
+          end
+          context "302レスポンスを返すこと" do
+            it "returns a 302 response" do
+              expect(response).to have_http_status "302"
+            end
+          end
+          context "ルート画面にリダイレクトすること" do
+            it "redirects to the root page" do
+              expect(response).to redirect_to root_path
+            end
+          end
+        end
+      end
+
+
 
       context "ゲストとして" do
         context "as a guest" do
@@ -386,8 +481,8 @@ RSpec.describe StudentsController, type: :controller do
           end
         end
       end
-      context "受講生としてログインしたとき" do
-        context "as a student" do
+      context "セラピスト養成コースの受講生としてログインしたとき" do
+        context "as a therapist training course student" do
           before do
             student = FactoryBot.create(:student)
             sign_in student
@@ -402,6 +497,23 @@ RSpec.describe StudentsController, type: :controller do
           end
         end
       end
+      context "セルフケアコースの受講生としてログインしたとき" do
+        context "as a self care coure student" do
+          before do
+            student = FactoryBot.create(:student, :self_care)
+            sign_in student
+            @another_student = FactoryBot.create(:student)
+          end
+          context "削除できないこと" do
+            it "does not delete student" do
+              expect {
+                delete :destroy, params: { id: @another_student.id }
+              }.to_not change(Student, :count)
+            end
+          end
+        end
+      end
+ 
       context "ゲストとして" do
         context "as a guest" do
           before do
