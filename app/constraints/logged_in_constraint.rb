3983b8806e529230f5b9dc6aa539c818a399af1d
ruffini47 <ruffini47@gmail.com>
2021-06-30 23:52:37 +0900

    sessionの一部書き換え

--- app/constraints/logged_in_constraint.rb
+++ app/constraints/logged_in_constraint.rb
@@ -1,5 +1,5 @@
 class LoggedInConstraint < Struct.new(:value)
   def matches?(request)
-    request.session['student_id'] == value || request.session['student_id'].nil?
+    request.session[:student_id] == value || request.session[:student_id].nil?
   end
 end
