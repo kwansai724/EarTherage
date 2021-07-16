82c468093eca4b2828a4fa9652148636998c4d98
kwansai724 <kwansai.taka724@gmail.com>
2021-07-16 22:15:19 +0900

    イベント・講座一覧が一般ユーザーでも閲覧出来るよう修正

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -7,7 +7,8 @@ class SchedulesController < ApplicationController
   def index
     if current_staff.present?
       @schedules = Schedule.paginate(page: params[:page], per_page: 6).order(created_at: "DESC")
-    elsif current_student.present?
+    # elsif current_student.present?
+    else
       @schedules = Schedule.paginate(page: params[:page], per_page: 6).where.not(public_status: "非公開").order(created_at: "DESC")
     end
     # if params[:search].present?
