2643513642f8e79f8118003a69f1170e85d4029d
kwansai724 <kwansai.taka724@gmail.com>
2021-07-28 22:39:11 +0900

    seedファイルを開発環境と本番環境で分ける

---
+++ db/seed/production.rb
@@ -0,0 +1,10 @@
+# 本番環境用
+Staff.create!(
+  name: "Minny Yamamoto*",
+  email: "office.anniversary@gmail.com",
+  admin: true,
+  password: "password",
+  password_confirmation: "password",
+)
+
+puts "Admin Created"
