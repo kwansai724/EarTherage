93535275934e3b1f2f06ef9aeaa0da47acd5c9fc
kwansai724 <kwansai.taka724@gmail.com>
2021-08-10 16:26:53 +0900

    S3連携の設定

--- app/uploaders/background_uploader.rb
+++ app/uploaders/background_uploader.rb
@@ -1,11 +1,15 @@
 class BackgroundUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.development? # 開発環境の場合
+    storage :file
+  elsif Rails.env.test? # テスト環境の場合
+    storage :file
+  else # 本番環境の場合
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -55,7 +59,7 @@ class BackgroundUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    original_filename if original_filename
+  end
 end
