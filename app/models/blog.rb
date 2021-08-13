db178dc8bee25a548b5102ad374ee0cc04b9a339
ruffini47 <ruffini47@gmail.com>
2021-08-13 10:15:57 +0900

    スタッフの名前による絞り込み＆公開状況_new,createでも機能するようにする

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,7 @@ class Blog < ApplicationRecord
   mount_uploader :image, BlogUploader
   validates :title, presence: true
   validates :datetime, presence: true
-  enum share_with: { "staff"=> 0, "therapist_training"=> 1, "self_care"=> 2, "general"=> 3, "temporary"=> 4 }, _prefix: true
+  enum share_with: { "staff"=> 0, "therapist_training"=> 1, "self_care"=> 2, "general"=> 3, "private"=> 4 }, _prefix: true
 
 #検索機能--------------------------------------------------------------------
   #閲覧可能範囲による絞り込み
