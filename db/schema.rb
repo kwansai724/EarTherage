9d0cfd4a493236bb3d1b0bd7438e082c66be743e
kwansai724 <kwansai.taka724@gmail.com>
2021-08-27 23:27:22 +0900

    スタッフ新規登録リンクをスタッフ一覧画面へ

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_08_10_073602) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title", default: "", null: false
-    t.datetime "datetime", default: "2021-08-16 03:12:45", null: false
+    t.datetime "datetime", default: "2021-08-16 08:02:49", null: false
     t.text "content", limit: 10485760
     t.string "image"
     t.integer "share_with", default: 0, null: false
